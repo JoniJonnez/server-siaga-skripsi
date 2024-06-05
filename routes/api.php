901abe0a4d09fa577be6bd9d7b70ed61b53800aa
ibnu <ibnuhamdani234@gmail.com>
2024-06-05 23:58:22 +0700

    update iuran list and upload image iuran

--- routes/api.php
+++ routes/api.php
@@ -33,3 +33,8 @@ Route::post('/user/pengaturan/{id}', 'App\Http\Controllers\Api\UserApiController
 Route::get('/informasi', 'App\Http\Controllers\Api\InformasiApiController@get_all');
 Route::get('/informasi/by-id/{id}', 'App\Http\Controllers\Api\InformasiApiController@by_id');
 Route::delete('/informasi/delete/{id}', 'App\Http\Controllers\Api\InformasiApiController@delete');
+
+Route::get('/iuran/list', 'App\Http\Controllers\Api\IuranApiController@get_all');
+// Route::get('/informasi/by-id/{id}', 'App\Http\Controllers\Api\InformasiApiController@by_id');
+// Route::delete('/informasi/delete/{id}', 'App\Http\Controllers\Api\InformasiApiController@delete');
+Route::post('/iuran/upload', 'App\Http\Controllers\Api\IuranApiController@insert');
